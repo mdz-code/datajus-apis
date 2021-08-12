4de6b56b075f660ef7da14e126ffa8fa105b210b
Leonardo Lopes dos Santos <lameranha@gmail.com>
2021-08-12 20:57:32 -0300

    🛰  apis da feature da issue #3 entregue

--- src/services/contract.js
+++ src/services/contract.js
@@ -1,12 +1,14 @@
-const queryBuilder = require('../models/supabase')
+const supabase = require('../models/supabase')
 const TemplateServices = require('./templates')
 
 
 class ContractServices {
     templateServices
+    supabase
 
     constructor() {
         this.templateServices = new TemplateServices()
+        this.supabase = supabase
     }
 
     async handlingRequest(req, res) {
@@ -16,12 +18,15 @@ class ContractServices {
         res.send(fileBytes)
     }
 
-    async renderContract(contractId) {
-        const { custom_json: customObject, document_id: documentId } = await queryBuilder('contracts', 'id', contractId, ['document_id', 'custom_json'])
-        const { body_html: htmlTemplate } = await queryBuilder('document_templates', 'document_id', documentId, ['body_html'])
+    async renderContract(contractId) {        
+        const { custom_json: customObject, document_id: documentId } = await this.supabase.queryBuilder('contracts', 'id', contractId, ['document_id', 'custom_json'])
+
+        const { body_html: htmlTemplate } = await this.supabase.queryBuilder('document_templates', 'document_id', documentId, ['body_html'])
         const htmlRender = await this.templateServices.renderByHtmlTemplate(htmlTemplate, customObject)
         return await this.templateServices.createPDF(htmlRender)
     }
+
+    
 }
 
 module.exports = ContractServices

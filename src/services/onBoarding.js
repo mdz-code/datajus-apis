3e025fdfcefa8c6f80ca8fc4aad56f0625b2b104
Leonardo Lopes dos Santos <lameranha@gmail.com>
2021-08-13 15:19:57 -0300

    🐋 new docker image

--- src/services/onBoarding.js
+++ src/services/onBoarding.js
@@ -2,4 +2,13 @@ const supabase = require('../models/supabase')
 
 module.exports = class OnBoarding {
 
+    async handlingRequest(req, res) {
+        const { uid: onBoardingId } = req.params
+        const foundedItens = await supabase.queryBuilder('form_itens', 'on_boarding_id', onBoardingId, ['label', 'type'])
+
+        res.json(foundedItens)
+    }
+
+    async getFormElements() {}
+
 }

916a23576127f143e34ace1f57cae3c8f0ecf178
Leonardo Lopes dos Santos <lameranha@gmail.com>
2021-08-13 15:34:11 -0300

    🔫 getForm feature

--- src/api/controller.js
+++ src/api/controller.js
@@ -1,14 +1,17 @@
 const router = require('express').Router()
 const TemplateServices = require('../services/templates')
 const ContractServices = require('../services/contract')
+const OnBoarding = require('../services/onBoarding')
 
 const templateServices = new TemplateServices()
 const contractServices = new ContractServices()
+const onBoarding = new OnBoarding()
 
 
 router.post('/createDocument', async (req, res) => await templateServices.handlingRequest(req, res))
-
 router.get('/getDocument/:uid', async (req, res) => await contractServices.handlingRequest(req, res))
+router.get('/getForm/:uid', async (req, res) => await onBoarding.handlingRequest(req, res))
+
 
 
 module.exports = router
--- src/services/onBoarding.js
+++ src/services/onBoarding.js
@@ -1,14 +1,15 @@
 const supabase = require('../models/supabase')
 
-module.exports = class OnBoarding {
+class OnBoarding {
 
     async handlingRequest(req, res) {
         const { uid: onBoardingId } = req.params
-        const foundedItens = await supabase.queryBuilder('form_itens', 'on_boarding_id', onBoardingId, ['label', 'type'])
+        const foundedItens = await supabase.queryBuilder('form_itens', 'on_boarding_id', onBoardingId, ['label', 'type'], 20)
 
         res.json(foundedItens)
     }
 
     async getFormElements() {}
-
 }
+
+module.exports = OnBoarding

bcfa25b0031507830a8deff576673e4c9b6e367a
Leonardo Lopes dos Santos <lameranha@gmail.com>
2021-08-15 12:51:08 -0300

    🍶 cpf na autenticação

--- src/services/signers.js
+++ src/services/signers.js
@@ -3,17 +3,20 @@ const supabase = require('../models/supabase')
 class Signers {
 
     async handlingRequest(req, res) {
-        const { completeName, email, phone } = req.body
-        const response = await this.storeSigner(completeName, email, phone)
+        const { nomeCompleto: completeName, email, cpf, telefone: phone } = req.body
+        console.log(req.body)
+        const response = await this.storeSigner(completeName, email, phone, cpf)
         res.send(response)
     }
 
-    async storeSigner(nome, email, phone) {
+    async storeSigner(nome, email, phone, cpf) {
+        console.log(nome)
 
         const arrayInset = [{
             complete_name: nome,
             email: email,
             phone: phone,
+            cpf: cpf
         }]
 
         const { id } = await supabase.insertValue('signers', arrayInset)
